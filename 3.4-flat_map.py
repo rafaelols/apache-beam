76a95969f893c637f3a64e3fbffae69b40af46d2
rafaelols <rafaelols94@gmail.com>
2021-06-16 19:44:22 -0300

    change result filename

--- 3.4-flat_map.py
+++ 3.4-flat_map.py
@@ -6,6 +6,6 @@ Collection = (
     p1
     |beam.io.ReadFromText('data/poema.txt')
     |beam.FlatMap(lambda record: record.split(' '))
-    |beam.io.WriteToText('data/resultado.txt')
+    |beam.io.WriteToText('data/resultado3.4.txt')
 )
 p1.run()
