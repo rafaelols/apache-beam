dce08a9f8b3d93184d4a172504d4ad2094f2c777
rafaelols <rafaelols94@gmail.com>
2021-06-16 20:12:05 -0300

    fix data folder

--- 3.9-CoGroupByKey.py
+++ 3.9-CoGroupByKey.py
@@ -4,7 +4,7 @@ p1 = beam.Pipeline()
 
 Tempo_Atrasos = (
   p1
-  | "Importar Dados Atraso" >> beam.io.ReadFromText("voos_sample.csv", skip_header_lines = 1)
+  | "Importar Dados Atraso" >> beam.io.ReadFromText("data/voos_sample.csv", skip_header_lines = 1)
   | "Separar por Vírgulas Atraso" >> beam.Map(lambda record: record.split(','))
   | "Pegar voos com atraso" >> beam.Filter(lambda record: int(record[8]) > 0 )
   | "Criar par atraso" >> beam.Map(lambda record: (record[4],int(record[8])))
@@ -14,7 +14,7 @@ Tempo_Atrasos = (
 
 Qtd_Atrasos = (
   p1
-  | "Importar Dados" >> beam.io.ReadFromText("voos_sample.csv", skip_header_lines = 1)
+  | "Importar Dados" >> beam.io.ReadFromText("data/voos_sample.csv", skip_header_lines = 1)
   | "Separar por Vírgulas" >> beam.Map(lambda record: record.split(','))
   | "Pegar voos com atraso qtd" >> beam.Filter(lambda record: int(record[8]) > 0 )
   | "Criar par qtd" >> beam.Map(lambda record: (record[4],int(record[8])))
